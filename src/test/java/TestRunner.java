8e5f66ceaf12f28dc880443da7a0255153682ba0
Name <Name@gma.com>
2020-07-20 20:27:24 +0300

    Added folders

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,15 @@
-//
-//
-//import cucumber.api.CucumberOptions;
-//import cucumber.api.junit.Cucumber;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions
-//        (features = "src/test/resources",
-//         glue = "src.test.cucumberDef"
-////tags = "@myTag")
-//                public class TestRunner{
-//        }
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = "stepDefinitions",
+        tags = "not @Defect",
+        plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm", "pretty", "json:target/cucumber-report/report.json"}
+)
+
+
+public class TestRunner {
+}
